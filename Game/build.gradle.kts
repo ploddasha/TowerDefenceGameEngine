cb0190a83c7bd5fabd4db9181f708b08df2b74d4
mkrikunov <m.krikunov@g.nsu.ru>
2023-12-26 10:10:53 +0700

    my last version

--- Game/build.gradle.kts
+++ Game/build.gradle.kts
@@ -1,7 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.7.10"
     application
     id("org.openjfx.javafxplugin") version "0.0.8"
+    kotlin("plugin.serialization") version "1.5.21"
+
 }
 
 group = "org.example"
@@ -22,14 +26,24 @@ dependencies {
     implementation("no.tornado:tornadofx:1.7.20")
     implementation("org.openjfx:javafx-controls:19.0.2.1")
     implementation("org.openjfx:javafx-fxml:19.0.2.1")
+    implementation("org.json:json:20230227")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.openjfx:javafx-media:14.0.2")
+    implementation("javazoom:jlayer:1.0.1")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-kotlin {
-    jvmToolchain(11)
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
+
+//kotlin {
+//    jvmToolchain(11)
+//}
 
 application {
     mainClass.set("MainKt")
--- Game/src/main/kotlin/view/GameView.kt
+++ Game/src/main/kotlin/view/GameView.kt
@@ -1,11 +1,37 @@
 package view
 
 import javafx.geometry.Pos
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import javafx.scene.paint.Color
 import javafx.scene.text.FontWeight
+import model.fromEditing.MobsModel
 import tornadofx.*
+import viewModel.createMobModel
 
 class GameView : View("Bashenki") {
+
+    val mobsModel: MobsModel by inject()
+
+    private val tileMap = mutableMapOf<Pair<Int, Int>, String>()
+
+    private val pauseMenuView = PauseMenuView()
+
+    val grass = "/configs/fromEditing/map/grass.png"
+    val sand = "/configs/fromEditing/map/sand.png"
+    val water = "/configs/fromEditing/map/water.png"
+    val city = "/configs/fromEditing/map/city.jpg"
+    private val tiles = listOf(
+        grass, sand, water, city
+    )
+
+    data class Tile(val x: Int, val y: Int, val width: Double, val height: Double)
+
+    private val tileSize = 20.0
+    private val numRows = 10
+    private val numCols = 10
+
+
     override val root = stackpane {
 
         addClass("game-stack-pane")
@@ -15,6 +41,10 @@ class GameView : View("Bashenki") {
             paddingTop = 10.0
             paddingRight = 7.0
             alignment = Pos.TOP_RIGHT
+
+            createMobModel(mobsModel)
+            //to show that read json works
+            //label(text = mobsModel.mobsList[0].cost.toString())
 
             button("Shop") {
                 style {
@@ -27,23 +57,79 @@ class GameView : View("Bashenki") {
                 }
 
                 action {
-                    add(ShopView())
+                    replaceWith(ShopView::class)
                 }
             }
             button("Pause") {
                 style {
                     fontSize = 14.px
                     padding = box(5.px, 10.px)
-                    paddingAll = 5.0
                     backgroundColor += Color.rgb(255, 152, 0)
                     textFill = Color.WHITE
                     fontWeight = FontWeight.BOLD
                 }
-
                 action {
-                    add(PauseMenuView())
+                    //pauseMenuView.root.isVisible = true
+                    replaceWith(PauseMenuView::class)
                 }
             }
+
+
+            gridpane {
+                hgap = 1.0
+                vgap = 1.0
+                paddingAll = 0.0
+
+                repeat(rowCount) { row ->
+                    repeat(columnCount) { col ->
+                        val tile = Tile(row, col, tileSize, tileSize)
+                        //val cellImageView = ImageView()
+                        //cellImageView.
+                        val tileRectangle = rectangle(tile.width, tile.height) {
+                            stroke = Color.BLACK
+                            fill = Color.TRANSPARENT
+
+                        }
+                        add(tileRectangle, col, row)
+                    }
+                }
+                alignment = javafx.geometry.Pos.CENTER
+            }
         }
+
+        /*
+        gridpane {
+            hgap = 1.0
+            vgap = 1.0
+            paddingAll = 0.0
+
+            repeat(20) { row ->
+                repeat(20) { col ->
+                    val cellImageView = ImageView(Image(resources.url(grass).toString()))
+                    cellImageView.isPreserveRatio = true
+                    cellImageView.fitWidth = 20.0
+                    cellImageView.fitHeight = 20.0
+                    cellImageView.opacity = 0.6
+                    tileMap[Pair(row, col)] = grass
+
+
+                    cellImageView.setOnMouseClicked {
+                        handleTileClick(cellImageView, col, row)
+                    }
+
+                    add(cellImageView, col, row)
+                }
+            }
+            alignment = javafx.geometry.Pos.CENTER
+
+        }
+        */
+
+
+
+    }
+
+    private fun handleTileClick(cellImageView: ImageView, col: Int, row: Int) {
+
     }
 }
